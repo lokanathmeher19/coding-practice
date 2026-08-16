f808cf2bde76da0de4dab1554dc8fa374f23bcd9
Lokanath Meher <meherlokanath314@gmail.com>
2026-08-16 19:30:36 +0530

    Java

---
+++ Java/07_Methods/ReverseMethod.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class ReverseMethod {
+
+    static int reverse(int num) {
+        int rev = 0;
+
+        while (num != 0) {
+            rev = rev * 10 + num % 10;
+            num /= 10;
+        }
+
+        return rev;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+
+        System.out.println(reverse(num));
+
+        sc.close();
+    }
+}
